48d4fa7ed77b63c79ea212208268facfba3afe68
Priyanka <priyanka.chowdhury2020@vitstudent.ac.in>
2022-01-22 12:31:36 +0530

    Firebase Auth + button animation

--- ChatAppKotlin/app/src/main/java/com/example/chatappkotlin/LoginActivity.kt
+++ ChatAppKotlin/app/src/main/java/com/example/chatappkotlin/LoginActivity.kt
@@ -2,10 +2,27 @@ package com.example.chatappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_login.setOnClickListener {
+            val email = et_login_email.text.toString()
+            val password = et_login_password.text.toString()
+            if(email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this,"Please enter email/pw",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this) {
+                    if (it.isSuccessful) Toast.makeText(this,"Successful",Toast.LENGTH_SHORT).show()
+                    else Toast.makeText(this,"Not successful",Toast.LENGTH_SHORT).show()
+                }
+        }
     }
 }
