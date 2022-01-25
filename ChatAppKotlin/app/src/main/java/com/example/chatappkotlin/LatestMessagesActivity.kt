0aa70d5c83eb00158526a6240106328ced2060ec
Priyanka <priyanka.chowdhury2020@vitstudent.ac.in>
2022-01-25 15:49:03 +0530

    ui + new chats

--- ChatAppKotlin/app/src/main/java/com/example/chatappkotlin/LatestMessagesActivity.kt
+++ ChatAppKotlin/app/src/main/java/com/example/chatappkotlin/LatestMessagesActivity.kt
@@ -13,6 +13,7 @@ class LatestMessagesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
 
+        supportActionBar?.title = "Galaxy Chat"
         verifyUserIsLoggedIn()
 
     }
--- ChatAppKotlin/app/src/main/java/com/example/chatappkotlin/NewMessagesActivity.kt
+++ ChatAppKotlin/app/src/main/java/com/example/chatappkotlin/NewMessagesActivity.kt
@@ -2,13 +2,24 @@ package com.example.chatappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_messages.*
+import kotlinx.android.synthetic.main.user_row_new_messages.view.*
+import androidx.recyclerview.widget.DividerItemDecoration
+
+
+
 
 class NewMessagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,23 +27,40 @@ class NewMessagesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_messages)
 
         supportActionBar?.title = "Select User"
-        val adapter = GroupAdapter<ViewHolder>()
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        rv_new_messages.adapter = adapter
-        //rv_new_messages.layoutManager = LinearLayoutManager(this) xml mai
+        rv_new_messages.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        fetchUsers()
+        
+    }
 
+    private  fun fetchUsers(){
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val adapter = GroupAdapter<ViewHolder>()
+                snapshot.children.forEach{
+                    Log.d("NewMessage",it.toString())
+                    val user= it.getValue(User::class.java)
+                    if (user!=null){
+                        adapter.add(UserItem(user))
+                    }
+                }
+                rv_new_messages.adapter = adapter
+
+            }
+        })
     }
 }
 
-class UserItem: Item<ViewHolder>(){
+class UserItem(val user:User): Item<ViewHolder>(){
     override fun bind(viewHolder: ViewHolder, position: Int) {
-
+        viewHolder.itemView.tv_profilename.text = user.username
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.iv_profileimg)
     }
-
     override fun getLayout(): Int {
         return R.layout.user_row_new_messages
     }
-
 }
